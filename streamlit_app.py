ff85e29b48cbcec1cf9f4bc6f1cbb5be56ac727f
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 13:00:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain import OpenAI, SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts.prompt import PromptTemplate
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 username = st.secrets["username"]
@@ -11,12 +12,33 @@ snowflake_account = st.secrets["account"]
 database = st.secrets["database"]
 schema = st.secrets["schema"]
 
+# setup
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tables=['merchant'])
 # llm = OpenAI(temperature=0) # using the following code to cache with gptcache
 llm = OpenAI(temperature=0, verbose=True)
 
-db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
+#prompt template
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+
+Only use the following tables:
+
+{table_info}
+
+If someone asks for the table employee, they really mean the merchant table.
+
+Question: {input}"""
+PROMPT = PromptTemplate(
+    input_variables=["input", "table_info", "dialect"], template=_DEFAULT_TEMPLATE
+)
+
+db_chain = SQLDatabaseChain(llm=llm, database=db, prompt=PROMPT, verbose=True)
 
 question = st.chat_input("How can I help you?")
 if question:
