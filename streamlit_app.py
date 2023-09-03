14a5419916057f0535b4a3f80c783a72da189cbd
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 12:28:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
+
+from gptcache.adapter.langchain_models import LangChainLLMs
+from gptcache.session import Session
+session = Session(name="sql-example") # set session for LangChainLLMs
+
 
