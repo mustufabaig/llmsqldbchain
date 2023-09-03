af70a7c1418ca96d0975cf3e60b06248df99b5e6
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 12:47:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 from langchain import OpenAI, SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 
-from gptcache.adapter.langchain_models import LangChainLLMs
-
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 username = st.secrets["username"]
 password = st.secrets["password"]
@@ -16,7 +14,8 @@ schema = st.secrets["schema"]
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['merchant'])
 # llm = OpenAI(temperature=0) # using the following code to cache with gptcache
-llm = LangChainLLMs(llm=OpenAI(temperature=0))
+llm = OpenAI(temperature=0, verbose=True)
 
 db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
 
+db_chain.run("How many employees are there?")
