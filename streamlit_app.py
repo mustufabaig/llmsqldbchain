8fc5136071aa7c8a6ec4aeb21052cf92ed82fbb6
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 12:34:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ from gptcache.adapter.langchain_models import LangChainLLMs
 from gptcache.session import Session
 session = Session(name="sql-example") # set session for LangChainLLMs
 
+snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
+# llm = OpenAI(temperature=0) # using the following code to cache with gptcache
+llm = LangChainLLMs(llm=OpenAI(temperature=0), session=session)
+
 
