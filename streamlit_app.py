d2d6df76fc1f2f004fc639d82b5f36875542e0f3
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 12:45:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ database = st.secrets["database"]
 schema = st.secrets["schema"]
 
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
+db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['merchant'])
 # llm = OpenAI(temperature=0) # using the following code to cache with gptcache
 llm = LangChainLLMs(llm=OpenAI(temperature=0))
 
