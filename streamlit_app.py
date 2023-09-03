d836f5bbd3c2889bf6b9cc8bbde55f95c2d4e4bb
mustufabaig <mustufa.baig@gmail.com>
2023-09-03 12:40:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,19 @@
+import streamlit as st
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 
 from gptcache.adapter.langchain_models import LangChainLLMs
 from gptcache.session import Session
 session = Session(name="sql-example") # set session for LangChainLLMs
+
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+warehouse = st.secrets["warehouse"]
+role = st.secrets["role"]
+snowflake_account = st.secrets["account"]
+database = st.secrets["database"]
+schema = st.secrets["schema"]
 
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=1, include_tables=['merchant','my_me_merchant_benchmark','my_peer_merchant_benchmark','my_region_merchant_benchmark'])
