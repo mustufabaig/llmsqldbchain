6cbccca8fa086363f42e24a345eb3e7880447da9
mustufabaig <mustufa.baig@gmail.com>
2023-09-04 22:01:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import json
 
-from langchain import OpenAI, SQLDatabase
+from langchain import SQLDatabase
+from langchain.chat_models import ChatOpenAI
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.prompts.prompt import PromptTemplate
 
@@ -22,7 +23,7 @@ schema = st.secrets["schema"]
 snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
 db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tables=['merchant','my_me_benchmark','my_peer_benchmark'])
 # llm = OpenAI(temperature=0) # using the following code to cache with gptcache
-llm = OpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
 
 #prompt template
 _DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer to the input question. 
