ed0670ab7b1b0945c3e93319cddff2cfe7426f25
mustufabaig <mustufa.baig@gmail.com>
2023-09-05 14:55:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tab
 llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
 
 #prompt template
-_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer to the input question. 
+_DEFAULT_TEMPLATE = """Answer the question based on the context below. If the question cannot be answered using the information provided answer with "I don't know". 
+
+Context: Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer to the input question. 
 Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database.
 Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
 Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
@@ -44,7 +46,8 @@ Only use the following tables:
 
 {table_info}
 
-If someone asks to compare data with peers then join my_me_benchmark table with my_peer_benchmark table and then compare the measures.
+If someone asks to compare data with peers then join my_me_benchmark table with my_peer_benchmark table and then compare the measures from both tables.
+If someone mentions "performance" then they really mean "volume".
 
 Question: {input}"""
 PROMPT = PromptTemplate(
