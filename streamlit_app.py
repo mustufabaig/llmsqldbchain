3f9e244f88d7e6c61b4694716c2f31b887d86561
mustufabaig <mustufa.baig@gmail.com>
2023-09-10 11:06:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,34 +12,42 @@ import fewshotprompttemplate
 #setting streamlit properties
 st.set_page_config(layout="wide")
 
-#loading config from streamlit settings
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-username = st.secrets["username"]
-password = st.secrets["password"]
-warehouse = st.secrets["warehouse"]
-role = st.secrets["role"]
-snowflake_account = st.secrets["account"]
-database = st.secrets["database"]
-schema = st.secrets["schema"]
+def get_db_chain():
+    if 'db_chain' not in st.session_state:
+        #loading config from streamlit settings
+        OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+        username = st.secrets["username"]
+        password = st.secrets["password"]
+        warehouse = st.secrets["warehouse"]
+        role = st.secrets["role"]
+        snowflake_account = st.secrets["account"]
+        database = st.secrets["database"]
+        schema = st.secrets["schema"]
+        
+        # setup
+        snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+        db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tables=['merchant','my_me_benchmark','my_peer_benchmark'])
+        # llm = OpenAI(temperature=0) # using the following code to cache with gptcache
+        llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
+        
+        #prompt template
+        # now create the few shot prompt template
+        few_shot_prompt_template = FewShotPromptTemplate(
+            examples=fewshotprompttemplate.examples,
+            example_prompt=fewshotprompttemplate.example_prompt,
+            prefix=fewshotprompttemplate.prefix,
+            suffix=fewshotprompttemplate.suffix,
+            input_variables=["input", "table_info", "dialect"],
+            example_separator="\n\n"
+        )
+        
+        local_db_chain = SQLDatabaseChain(llm=llm, database=db, prompt=few_shot_prompt_template, verbose=True, top_k=3, use_query_checker=True, return_intermediate_steps=True)
+        st.session_state['db_chain'] = local_db_chain
+        return local_db_chain
+        
+    return st.session_state['db_chain']
 
-# setup
-snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
-db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tables=['merchant','my_me_benchmark','my_peer_benchmark'])
-# llm = OpenAI(temperature=0) # using the following code to cache with gptcache
-llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
-
-#prompt template
-# now create the few shot prompt template
-few_shot_prompt_template = FewShotPromptTemplate(
-    examples=fewshotprompttemplate.examples,
-    example_prompt=fewshotprompttemplate.example_prompt,
-    prefix=fewshotprompttemplate.prefix,
-    suffix=fewshotprompttemplate.suffix,
-    input_variables=["input", "table_info", "dialect"],
-    example_separator="\n\n"
-)
-
-db_chain = SQLDatabaseChain(llm=llm, database=db, prompt=few_shot_prompt_template, verbose=True, top_k=3, use_query_checker=True, return_intermediate_steps=True)
+db_chain = get_db_chain()
 
 question = st.chat_input("How can I help you?")
 if question:
