e9cb5b00b9551100b7be3b625e96a00aa8a2ca84
mustufabaig <mustufa.baig@gmail.com>
2023-10-14 10:08:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,4 @@ if question:
         except Exception as error:
             with st.chat_message("assistant"):
                 st.write("I don't think I can answer your question - try a different question.")
-                st.write(error._message)
-                st.write(error._sql_message)
-                st.write(error.detail)
                 st.write(vars(error))
