7380f05df7236dcfa6bae467ce042174505a67ef
mustufabaig <mustufa.baig@gmail.com>
2023-11-12 15:45:28 -0500

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -4,6 +4,9 @@ Here is the schema of the tables you will be querying:
 {table_info}
 
 Always use parent_aggregate_merchant_id=10000111
+If someone asks to compare data with peers then join my_me_benchmark table with my_peer_benchmark table and then compare the measures from both tables.
+If someone mentions "performance" then they really mean "volume".
+If someone ask for "my chargeback" or "my Fraud" or "my declined" then use table my_me_benchmark.
 
 You must always output your answer in JSON format with the following key-value pairs:
 - "query": the SQL query that you generated
