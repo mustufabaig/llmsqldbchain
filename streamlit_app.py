d879e96be3a21eafefe5cbc5bff2be9c54fedf84
mustufabaig <mustufa.baig@gmail.com>
2023-11-12 23:03:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,8 @@ if question:
                 pretty_json = json.dumps(answer["intermediate_steps"], indent=4)
                 st.code(answer["intermediate_steps"][5])
                 jdata = answer["intermediate_steps"][5]
-                df = pd.json_normalize(jdata["data-result"])
+                st.write(jdata)
+                #df = pd.json_normalize(jdata["data-result"])
             with st.expander("Click here for details"):
                 #st.text(answer["intermediate_steps"][1])
                 st.text(json.dumps(answer["intermediate_steps"], indent=4))
