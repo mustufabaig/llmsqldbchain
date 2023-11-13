d9f8000c87e84a4ee4d486b2b5624dfd73b61b49
mustufabaig <mustufa.baig@gmail.com>
2023-11-12 22:18:52 -0500

    Update fewshotprompttemplate.py

--- fewshotprompttemplate.py
+++ fewshotprompttemplate.py
@@ -50,7 +50,7 @@ You must always output your answer in JSON format with the following key-value p
 - "input": input question
 - "query": the SQL query that you generated
 - "error": an error message if the query is invalid, or null if the query is valid
-- "data-result" : SQL query results
+- "data-result" : SQL query results with column names in capitals
 - "text-result" : show SQL query results in natural language
 
 Only use the following tables:
