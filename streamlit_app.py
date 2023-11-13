432869ea10f6763e8a7020089affc2f69a2ed68e
mustufabaig <mustufa.baig@gmail.com>
2023-11-13 07:44:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ def get_db_chain():
         OPENAI_API_BASE = st.secrets["OPENAI_API_BASE"]
         OPENAI_API_TYPE = st.secrets["OPENAI_API_TYPE"]
         OPENAI_API_VERSION = st.secrets["OPENAI_API_VERSION"]
-        OPENAI_CHAT_MODEL = st.secrets["OPENAI_CHAT_MODEL_35"]
-        MODEL_DEPLOYMENT_NAME = st.secrets["MODEL_DEPLOYMENT_NAME_35"]
+        OPENAI_CHAT_MODEL = st.secrets["OPENAI_CHAT_MODEL_40"]
+        MODEL_DEPLOYMENT_NAME = st.secrets["MODEL_DEPLOYMENT_NAME_40"]
         
         username = st.secrets["username"]
         password = st.secrets["password"]
@@ -41,7 +41,7 @@ def get_db_chain():
         db = SQLDatabase.from_uri(snowflake_url,sample_rows_in_table_info=3, include_tables=['merchant','my_me_benchmark','my_peer_benchmark'])
         #llm = OpenAI(temperature=0) # using the following code to cache with gptcache
         #llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', verbose=True)
-        llm = AzureChatOpenAI(temperature=0, deployment_name=MODEL_DEPLOYMENT_NAME, model=OPENAI_CHAT_MODEL, verbose=True)
+        llm = AzureChatOpenAI(temperature=0.2, deployment_name=MODEL_DEPLOYMENT_NAME, model=OPENAI_CHAT_MODEL, verbose=True)
         
         #prompt template
         # now create the few shot prompt template
